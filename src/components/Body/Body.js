b6121b9e05f81286911aeffeed25046922fcd27b
tk <trishank01@gmail.com>
2022-12-15 20:48:16 +0530

    added resume editor UI

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Body.module.css'
 import {AiOutlineArrowDown} from 'react-icons/ai'
-
+import Editor from '../Editor/Editor';
 const Body = () => {
     const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
+
+
+    const sections = {
+        basicInfo: "Basic Info",
+        workExp: "Work Experience",
+        project: "Projects",
+        education: "Education",
+        achievement: "Achievements",
+        summary: "Summary",
+        other: "Other",
+      };
+
+  
+      const [resumeInformation, setResumeInformation] = useState({
+        [sections.basicInfo]: {
+          id: sections.basicInfo,
+          sectionTitle: sections.basicInfo,
+          detail: {},
+        },
+        [sections.workExp]: {
+          id: sections.workExp,
+          sectionTitle: sections.workExp,
+          details: [],
+        },
+        [sections.project]: {
+          id: sections.project,
+          sectionTitle: sections.project,
+          details: [],
+        },
+        [sections.education]: {
+          id: sections.education,
+          sectionTitle: sections.education,
+          details: [],
+        },
+        [sections.achievement]: {
+          id: sections.achievement,
+          sectionTitle: sections.achievement,
+          points: [],
+        },
+        [sections.summary]: {
+          id: sections.summary,
+          sectionTitle: sections.summary,
+          detail: "",
+        },
+        [sections.other]: {
+          id: sections.other,
+          sectionTitle: sections.other,
+          detail: "",
+        },
+      });
+
+      console.log(resumeInformation)
+
+
   return (
     <div className={styles.container}>
      <p className={styles.heading}>Resume Builder</p>
@@ -16,6 +70,10 @@ const Body = () => {
          })}
         </div>
         <button>Download <AiOutlineArrowDown/> </button>
+     </div>
+     <div className={styles.main}>
+      <Editor sections={sections} information={resumeInformation}/>
+      
      </div>
     </div>
   )
---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,415 @@
+import React, { useEffect, useState } from "react";
+import InputControl from "../InputControl/InputControl";
+import { CiCircleRemove } from "react-icons/ci";
+import styles from "./Editor.module.css";
+const Editor = ({ sections, information }) => {
+  const [activeSectionKey, setActiveSectionKey] = useState(
+    Object.keys(sections)[0]
+  );
+
+  const [activeInformantion, setActiveInformantion] = useState(
+    information[sections[Object.keys(sections)[0]]]
+  );
+
+  const [sectionTitle, setSectionTitle] = useState(
+    sections[Object.keys(sections)[0]]
+  );
+
+  const [values, setValues] = useState({
+    name : activeInformantion?.detail?.name || "",
+    title : activeInformantion?.detail?.title || "",
+    linkedin : activeInformantion?.detail?.linkedin || "",
+    github : activeInformantion?.detail?.github || "",
+    phone : activeInformantion?.detail?.phone || "",
+    email : activeInformantion?.detail?.email || "",
+  });
+
+  const handlePointUpdate = (value , index) => {
+       const tempValues = {...values};
+       tempValues.points[index] = value;
+       setValues(tempValues)
+  };
+
+  const workExpBody = (
+    <div className={styles.detail}>
+      <div className={styles.row}>
+        <InputControl
+          label="Title"
+          placeholder="Enter title eg. Frontend developer"
+          value={values.title}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, title: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Company Name"
+          placeholder="Enter company name eg. amazon"
+          value={values.companyName}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, companyName: event.target.value }))
+          }
+        />
+      </div>
+      <div className={styles.row}>
+        <InputControl
+          label="Certificate Link"
+          placeholder="Enter certificate link"
+          value={values.certificationLink}
+          onChange={(event) =>
+            setValues((prev) => ({
+              ...prev,
+              certificationLink: event.target.value,
+            }))
+          }
+        />
+        <InputControl
+          label="Location"
+          placeholder="Enter location eg. Remote"
+          value={values.location}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, location: event.target.value }))
+          }
+        />
+      </div>
+      <div className={styles.row}>
+        <InputControl
+          label="Start Date"
+          type="date"
+          placeholder="Enter start date of work"
+          value={values.startDate}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, startDate: event.target.value }))
+          }
+        />
+        <InputControl
+          label="End Date"
+          type="date"
+          placeholder="Enter end date of work"
+          value={values.endDate}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, endDate: event.target.value }))
+          }
+        />
+      </div>
+
+      <div className={styles.column}>
+        <label>Enter work description</label>
+        <InputControl
+          placeholder="Line 1"
+          value={values.points ? values.points[0] : ""}
+          onChange={(event) => handlePointUpdate(event.target.value, 0)}
+        />
+        <InputControl
+          placeholder="Line 2"
+          value={values.points ? values.points[1] : ""}
+          onChange={(event) => handlePointUpdate(event.target.value, 1)}
+        />
+        <InputControl
+          placeholder="Line 3"
+          value={values.points ? values.points[2] : ""}
+          onChange={(event) => handlePointUpdate(event.target.value, 2)}
+        />
+      </div>
+    </div>
+  );
+  const projectBody = (
+    <div className={styles.detail}>
+      <div className={styles.row}>
+        <InputControl
+          label="Title"
+          value={values.title}
+          placeholder="Enter title eg. Chat app"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, title: event.target.value }))
+          }
+        />
+      </div>
+      <InputControl
+        label="Overview"
+        value={values.overview}
+        placeholder="Enter basic overview of project"
+        onChange={(event) =>
+          setValues((prev) => ({ ...prev, overview: event.target.value }))
+        }
+      />
+      <div className={styles.row}>
+        <InputControl
+          label="Deployed Link"
+          value={values.link}
+          placeholder="Enter deployed link of project"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, link: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Github Link"
+          value={values.github}
+          placeholder="Enter github link of project"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, github: event.target.value }))
+          }
+        />
+      </div>
+      <div className={styles.column}>
+        <label>Enter project description</label>
+        <InputControl
+          placeholder="Line 1"
+          value={values.points ? values.points[0] : ""}
+          onChange={(event) => handlePointUpdate(event.target.value, 0)}
+        />
+        <InputControl
+          placeholder="Line 2"
+          value={values.points ? values.points[1] : ""}
+          onChange={(event) => handlePointUpdate(event.target.value, 1)}
+        />
+        <InputControl
+          placeholder="Line 3"
+          value={values.points ? values.points[2] : ""}
+          onChange={(event) => handlePointUpdate(event.target.value, 2)}
+        />
+        <InputControl
+          placeholder="Line 4"
+          value={values.points ? values.points[3] : ""}
+          onChange={(event) => handlePointUpdate(event.target.value, 3)}
+        />
+      </div>
+    </div>
+  );
+  const educationBody = (
+    <div className={styles.detail}>
+      <div className={styles.row}>
+        <InputControl
+          label="Title"
+          value={values.title}
+          placeholder="Enter title eg. B-tech"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, title: event.target.value }))
+          }
+        />
+      </div>
+      <InputControl
+        label="College/School Name"
+        value={values.college}
+        placeholder="Enter name of your college/school"
+        onChange={(event) =>
+          setValues((prev) => ({ ...prev, college: event.target.value }))
+        }
+      />
+      <div className={styles.row}>
+        <InputControl
+          label="Start Date"
+          type="date"
+          placeholder="Enter start date of this education"
+          value={values.startDate}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, startDate: event.target.value }))
+          }
+        />
+        <InputControl
+          label="End Date"
+          type="date"
+          placeholder="Enter end date of this education"
+          value={values.endDate}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, endDate: event.target.value }))
+          }
+        />
+      </div>
+    </div>
+  );
+  const basicInfoBody = (
+    <div className={styles.detail}>
+      <div className={styles.row}>
+        <InputControl
+          label="Name"
+          placeholder="Enter your full name eg. Aashu"
+          value={values.name}
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, name: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Title"
+          value={values.title}
+          placeholder="Enter your title eg. Frontend developer"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, title: event.target.value }))
+          }
+        />
+      </div>
+      <div className={styles.row}>
+        <InputControl
+          label="Linkedin Link"
+          value={values.linkedin}
+          placeholder="Enter your linkedin profile link"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, linkedin: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Github Link"
+          value={values.github}
+          placeholder="Enter your github profile link"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, github: event.target.value }))
+          }
+        />
+      </div>
+      <div className={styles.row}>
+        <InputControl
+          label="Email"
+          value={values.email}
+          placeholder="Enter your email"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Enter phone"
+          value={values.phone}
+          placeholder="Enter your phone number"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, phone: event.target.value }))
+          }
+        />
+      </div>
+    </div>
+  );
+  const achievementsBody = (
+    <div className={styles.detail}>
+      <div className={styles.column}>
+        <label>List your achievements</label>
+        <InputControl
+          placeholder="Line 1"
+          value={values.points ? values.points[0] : ""}
+          onChange={(event) => handlePointUpdate(event.target.value, 0)}
+        />
+        <InputControl
+          placeholder="Line 2"
+          value={values.points ? values.points[1] : ""}
+          onChange={(event) => handlePointUpdate(event.target.value, 1)}
+        />
+        <InputControl
+          placeholder="Line 3"
+          value={values.points ? values.points[2] : ""}
+          onChange={(event) => handlePointUpdate(event.target.value, 2)}
+        />
+        <InputControl
+          placeholder="Line 4"
+          value={values.points ? values.points[3] : ""}
+          onChange={(event) => handlePointUpdate(event.target.value, 3)}
+        />
+      </div>
+    </div>
+  );
+  const summaryBody = (
+    <div className={styles.detail}>
+      <InputControl
+        label="Summary"
+        value={values.summary}
+        placeholder="Enter your objective/summary"
+        onChange={(event) =>
+          setValues((prev) => ({ ...prev, summary: event.target.value }))
+        }
+      />
+    </div>
+  );
+  const otherBody = (
+    <div className={styles.detail}>
+      <InputControl
+        label="Other"
+        value={values.other}
+        placeholder="Enter something"
+        onChange={(event) =>
+          setValues((prev) => ({ ...prev, other: event.target.value }))
+        }
+      />
+    </div>
+  );
+
+  const handleSubmisson = () => {
+    console.log(values)
+  }
+
+
+  useEffect(() => {
+    const activeInfo =information[sections[activeSectionKey]]
+    setActiveInformantion(activeInfo);
+    setSectionTitle(sections[activeSectionKey]);
+    setValues({
+        name : activeInformantion?.detail?.name || "",
+        title : activeInformantion?.detail?.title || "",
+        linkedin : activeInformantion?.detail?.linkedin || "",
+        github : activeInformantion?.detail?.github || "",
+        phone : activeInformantion?.detail?.phone || "",
+        email : activeInformantion?.detail?.email || "",
+      })
+  }, [information, activeSectionKey, sections , activeInformantion]);
+
+  const generateBody = () => {
+    switch (sections[activeSectionKey]) {
+      case sections.basicInfo:
+        return basicInfoBody;
+      case sections.workExp:
+        return workExpBody;
+      case sections.project:
+        return projectBody;
+      case sections.education:
+        return educationBody;
+      case sections.achievement:
+        return achievementsBody;
+      case sections.summary:
+        return summaryBody;
+      case sections.other:
+        return otherBody;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        {Object.keys(sections)?.map((key) => {
+          return (
+            <div
+              className={`${styles.section} ${
+                activeSectionKey === key ? styles.active : ""
+              }`}
+              key={key}
+              onClick={() => setActiveSectionKey(key)}
+            >
+              {sections[key]}
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.body}>
+        <InputControl
+          label="Title"
+          placeholder="Enter section title"
+          value={sectionTitle}
+          onChange={(e) => setSectionTitle(e.target.value)}
+        />
+        <div className={styles.chips}>
+          {activeInformantion?.details
+            ? activeInformantion?.details.map((item, index) => (
+                <div className={styles.chip} key={item.title + index}>
+                  <p>
+                    {sections[activeSectionKey]} {index + 1}
+                  </p>
+                  <CiCircleRemove />
+                </div>
+              ))
+            : ""}
+        </div>
+        {generateBody()}
+      <button onClick={handleSubmisson}>Save</button>
+      </div>
+
+     
+    </div>
+  );
+};
+
+export default Editor;
